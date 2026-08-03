ce8357fdd405da67b284e9e4e8189f0297bf6e81
Hungdinhz <phunghung12112005@gmail.com>
2026-08-03 21:57:38 +0700

    feat: add analytics reports, implement programming exercise sandbox execution, and introduce user role management page

--- lms/lms/report/certificate_report/certificate_report.py
+++ lms/lms/report/certificate_report/certificate_report.py
@@ -14,20 +14,77 @@ def get_columns():
 		{
 			"fieldname": "name",
 			"fieldtype": "Link",
-			"label": _("ID"),
+			"label": _("Certificate ID"),
 			"options": "LMS Certificate",
+			"width": 160,
+		},
+		{
+			"fieldname": "member",
+			"fieldtype": "Link",
+			"label": _("Learner"),
+			"options": "User",
+			"width": 150,
+		},
+		{
+			"fieldname": "member_name",
+			"fieldtype": "Data",
+			"label": _("Learner Name"),
+			"width": 160,
+		},
+		{
+			"fieldname": "course",
+			"fieldtype": "Link",
+			"label": _("Course"),
+			"options": "LMS Course",
+			"width": 150,
+		},
+		{
+			"fieldname": "course_title",
+			"fieldtype": "Data",
+			"label": _("Course Title"),
 			"width": 200,
-		}
+		},
+		{
+			"fieldname": "issue_date",
+			"fieldtype": "Date",
+			"label": _("Issue Date"),
+			"width": 120,
+		},
+		{
+			"fieldname": "expiry_date",
+			"fieldtype": "Date",
+			"label": _("Expiry Date"),
+			"width": 120,
+		},
+		{
+			"fieldname": "template",
+			"fieldtype": "Data",
+			"label": _("Template"),
+			"width": 140,
+		},
 	]
 
 def get_data(filters=None):
-	conditions = ""
-	# TODO: Add dynamic filters based on filters dict
+	query_filter = {}
+	if filters:
+		if filters.get("course"):
+			query_filter["course"] = filters.get("course")
+		if filters.get("member"):
+			query_filter["member"] = filters.get("member")
+		if filters.get("from_date"):
+			query_filter["issue_date"] = [">=", filters.get("from_date")]
+		if filters.get("to_date"):
+			if "issue_date" in query_filter:
+				query_filter["issue_date"] = ["between", (filters.get("from_date"), filters.get("to_date"))]
+			else:
+				query_filter["issue_date"] = ["<=", filters.get("to_date")]
 	
 	records = frappe.get_all(
 		"LMS Certificate",
-		fields=["name"],
-		filters=filters
+		fields=["name", "member", "member_name", "course", "course_title", "issue_date", "expiry_date", "template"],
+		filters=query_filter,
+		order_by="issue_date desc"
 	)
 	
 	return records
+
--- lms/lms/report/learner_detail_report/learner_detail_report.py
+++ lms/lms/report/learner_detail_report/learner_detail_report.py
@@ -12,22 +12,75 @@ def execute(filters=None):
 def get_columns():
 	return [
 		{
-			"fieldname": "name",
+			"fieldname": "member",
 			"fieldtype": "Link",
-			"label": _("ID"),
-			"options": "LMS Enrollment",
-			"width": 200,
-		}
+			"label": _("Learner"),
+			"options": "User",
+			"width": 150,
+		},
+		{
+			"fieldname": "member_name",
+			"fieldtype": "Data",
+			"label": _("Learner Name"),
+			"width": 160,
+		},
+		{
+			"fieldname": "department",
+			"fieldtype": "Data",
+			"label": _("Department"),
+			"width": 140,
+		},
+		{
+			"fieldname": "course",
+			"fieldtype": "Link",
+			"label": _("Course"),
+			"options": "LMS Course",
+			"width": 160,
+		},
+		{
+			"fieldname": "progress",
+			"fieldtype": "Percent",
+			"label": _("Progress (%)"),
+			"width": 110,
+		},
+		{
+			"fieldname": "current_lesson",
+			"fieldtype": "Link",
+			"label": _("Current Lesson"),
+			"options": "Course Lesson",
+			"width": 160,
+		},
+		{
+			"fieldname": "creation",
+			"fieldtype": "Date",
+			"label": _("Enrollment Date"),
+			"width": 130,
+		},
 	]
 
 def get_data(filters=None):
-	conditions = ""
-	# TODO: Add dynamic filters based on filters dict
+	query_filter = {}
+	if filters and filters.get("member"):
+		query_filter["member"] = filters.get("member")
 	
 	records = frappe.get_all(
 		"LMS Enrollment",
-		fields=["name"],
-		filters=filters
+		fields=["name", "member", "member_name", "course", "progress", "current_lesson", "creation"],
+		filters=query_filter,
+		order_by="creation desc"
 	)
 	
-	return records
+	data = []
+	for r in records:
+		dept = frappe.db.get_value("User", r.member, "department", ignore=True) or ""
+		data.append({
+			"member": r.member,
+			"member_name": r.member_name,
+			"department": dept,
+			"course": r.course,
+			"progress": r.progress or 0,
+			"current_lesson": r.current_lesson or "",
+			"creation": r.creation.strftime("%Y-%m-%d") if r.creation else ""
+		})
+	return data
+
